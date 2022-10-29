ebb20907af669128bcbd64ac6e4e81c7e2a3ac72
Bvallegc <116285501+Bvallegc@users.noreply.github.com>
2022-10-29 20:18:14 +0200

    kjgitu

---
+++ tuberculo.py
@@ -0,0 +1,32 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Tuberculo(Sprite):
+    """A class to represent a single alien in the fleet."""
+
+    def __init__(self, ai_game):
+        """Initialize the alien and set its starting position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        # Load the alien image and set its rect attribute.
+        self.image = pygame.image.load('images/Monster.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+
+    def update(self):
+        """Move the alien right or left."""
+        self.y += self.settings.tuberculo_speed
+        self.rect.y = self.y
+
+
+
