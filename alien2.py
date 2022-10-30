e3ed4ef1147810d36e6499d3c1e60fcacb8c065f
yousif <yousif@pop-os.localdomain>
2022-10-30 15:52:36 +0100

    pu

--- alien2.py
+++ alien2.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
  
-class Alien(Sprite):
+class Alien2(Sprite):
     """A class to represent a single alien in the fleet."""
 
     def __init__(self, ai_game):
@@ -10,18 +10,19 @@ class Alien(Sprite):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
 
-        # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load('images/alien.bmp')
-        IMAGESIZE = (75,75)
+        self.image = pygame.image.load('images/powerup.bmp')
+        IMAGESIZE = (100,100)
         self.image = pygame.transform.scale(self.image, IMAGESIZE)
         self.rect = self.image.get_rect()
 
-        # Start each new alien near the top left of the screen.
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
+        self.rect.x = self.rect.width 
+        self.rect.y = self.rect.height 
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        self.direction = 1
+        self.lives = 3
 
     def check_edges(self):
         """Return True if alien is at edge of screen."""
@@ -29,8 +30,12 @@ class Alien(Sprite):
         if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
 
+
+        
+
     def update(self):
         """Move the alien right or left."""
-        self.x += (self.settings.alien_speed *
-                        self.settings.fleet_direction)
+        self.x += (self.settings.special_alien_speed * self.direction)
         self.rect.x = self.x
+        self.y += ((self.settings.alien_speed / 8))
+        self.rect.y = self.y
