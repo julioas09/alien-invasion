d8fcdb5ab5eba2ebc2ecbd2b34b50a85f58eb520
tvintimilla01 <tvintimilla.ieu2020@student.ie.edu>
2022-11-03 01:11:58 +0100

    newAlien commit

--- alien.py
+++ alien.py
@@ -32,3 +32,45 @@ class Alien(Sprite):
         self.x += (self.settings.alien_speed *
                         self.settings.fleet_direction)
         self.rect.x = self.x
+
+class newAlien(Sprite):
+    """A class to represent a single alien in the fleet."""
+
+    def __init__(self, ai_game):
+        """Initialize the alien and set its starting position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        # Load the alien image and set its rect attribute.
+        self.image = pygame.image.load('images/new_alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact horizontal position.
+        self.x = float(self.rect.x)
+        self.direction = 8
+        self.hit_points = 3
+
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Move the alien right or left."""
+        self.x += (self.settings.new_alien_speed *
+                        self.direction)
+        self.rect.x = self.x
+
+    def hits(self):
+        self.hit_points -= 1
+
+        if self.hit_points <= 0:
+            self.kill()
+            return True
+
